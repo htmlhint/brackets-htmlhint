855ef2af42555e4d2c78ba62d2bd2e99f1ee03b1
Raymond Camden <raymondcamden@gmail.com>
2013-11-01 09:57:07 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,15 @@
+brackets-htmlhint
+=================
+
+A Brackets wrapper for the HTMLHint library (see credit below).
+
+Issues/Updates
+=====
+[10/15/2013] So - the main CSSLint lib I used totally screwed up Brackets by messing up the exports module. I did
+some guess work and - I think it's kosher now.
+
+[
+
+Credit
+=====
+Built with [HTMLHint](http://htmlhint.com/). 
---
+++ main.js
@@ -0,0 +1,62 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define, brackets, $, window, CSSLint, Mustache */
+
+define(function (require, exports, module) {
+	'use strict';
+
+	var AppInit                 = brackets.getModule("utils/AppInit"),
+		CodeInspection			= brackets.getModule("language/CodeInspection");
+
+	require("htmlhint/htmlhint");
+
+	function htmlHinter(text, fullPath) {
+		var results;
+console.log('run htmlhinter');
+		results = HTMLHint.verify(text);
+console.dir(results);
+		if (results.length) {
+
+			var result = { errors: [] };
+
+			for(var i=0, len=results.length; i<len; i++) {
+
+				var messageOb = results[i];
+
+				if(!messageOb.line) continue;
+				//default
+				var type = CodeInspection.Type.WARNING;
+
+				if(messageOb.type === "error") {
+					type = CodeInspection.Type.ERROR;
+				} else if(messageOb.type === "warning") {
+					type = CodeInspection.Type.WARNING;
+				}
+
+				result.errors.push({
+					pos: {line:messageOb.line-1, ch:messageOb.col},
+					message:messageOb.message,
+					
+					type:type
+				});
+				
+			}
+			
+			return result;
+		} else {
+			//no errors
+			return null;
+		}
+
+	}
+
+	CodeInspection.register("html", {
+		name: "HTMLHint",
+		scanFile: htmlHinter
+	});
+	CodeInspection.register("htm", {
+		name: "HTMLHint",
+		scanFile: htmlHinter
+	});
+
+
+});
