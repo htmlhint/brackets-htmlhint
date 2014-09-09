3bd4bd39d88c8a16d5c8ae203207b9a4b38da76a
Diogo Gomes <dgomes@graphnode.com>
2014-09-09 02:26:18 +0100

    Added support for .htmlhintrc

--- main.js
+++ main.js
@@ -5,56 +5,94 @@ define(function (require, exports, module) {
 	'use strict';
 
 	var AppInit                 = brackets.getModule("utils/AppInit"),
-		CodeInspection			= brackets.getModule("language/CodeInspection");
-
+		CodeInspection			= brackets.getModule("language/CodeInspection"),
+        FileSystem              = brackets.getModule("filesystem/FileSystem"),
+        FileUtils               = brackets.getModule("file/FileUtils"),
+        ProjectManager          = brackets.getModule("project/ProjectManager");
+        
 	require("htmlhint/htmlhint");
 
+    var configFileName = ".htmlhintrc";
+    
 	function htmlHinter(text, fullPath) {
-		var results;
-		results = HTMLHint.verify(text);
+        return _loadRules().then(function (rules) {
+            var results;
+            
+            results = HTMLHint.verify(text, rules);
 
-		if (results.length) {
+            if (results.length) {
 
-			var result = { errors: [] };
+                var result = { errors: [] };
 
-			for(var i=0, len=results.length; i<len; i++) {
+                for(var i=0, len=results.length; i<len; i++) {
 
-				var messageOb = results[i];
+                    var messageOb = results[i];
 
-				if(!messageOb.line) continue;
-				//default
-				var type = CodeInspection.Type.WARNING;
+                    if(!messageOb.line) continue;
+                    //default
+                    var type = CodeInspection.Type.WARNING;
 
-				if(messageOb.type === "error") {
-					type = CodeInspection.Type.ERROR;
-				} else if(messageOb.type === "warning") {
-					type = CodeInspection.Type.WARNING;
-				}
+                    if(messageOb.type === "error") {
+                        type = CodeInspection.Type.ERROR;
+                    } else if(messageOb.type === "warning") {
+                        type = CodeInspection.Type.WARNING;
+                    }
 
-				result.errors.push({
-					pos: {line:messageOb.line-1, ch:messageOb.col},
-					message:messageOb.message,
-					
-					type:type
-				});
-				
-			}
-			
-			return result;
-		} else {
-			//no errors
-			return null;
-		}
-
+                    result.errors.push({
+                        pos: {line:messageOb.line-1, ch:messageOb.col},
+                        message:messageOb.message,
+                        
+                        type:type
+                    });
+                    
+                }
+                
+                return result;
+            } else {
+                //no errors
+                return null;
+            }
+        });
 	}
 
+    function _loadRules() {
+        var result = new $.Deferred();
+        
+        var projectRootEntry = ProjectManager.getProjectRoot(),
+            
+        if (!projectRootEntry) {
+            return result.resolve(undefined).promise();
+        }
+            
+        var file = FileSystem.getFileForPath(projectRootEntry.fullPath + configFileName);
+        file.read(function (err, content) {
+            if (!err) {
+                result.resolve(undefined);
+                return;
+            }
+            
+            var config;
+            
+            try {
+                config = JSON.parse(content);
+            } catch (e) {
+                console.error("HTMLHint: error parsing " + file.fullPath + ". Details: " + e);
+                result.reject(e);
+                return;
+            }
+            
+            result.resolve(config);
+        });
+        return result.promise();
+    }
+    
 	CodeInspection.register("html", {
 		name: "HTMLHint",
-		scanFile: htmlHinter
+		scanFileAsync: htmlHinter
 	});
 	CodeInspection.register("htm", {
 		name: "HTMLHint",
-		scanFile: htmlHinter
+		scanFileAsync: htmlHinter
 	});
 
 
